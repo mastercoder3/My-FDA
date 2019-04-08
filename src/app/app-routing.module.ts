c627bd79193e922533b57b831a1925890c0c7164
Muhammad Junaid <muhammadjunaidhassan1@gmail.com>
2019-04-08 20:37:48 +0500

    starting backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,20 +7,10 @@ const routes: Routes = [
     redirectTo: 'tabs',
     pathMatch: 'full'
   },
-  // {
-  //   path: 'home',
-  //   loadChildren: './home/home.module#HomePageModule'
-  // },
-  // {
-  //   path: 'list',
-  //   loadChildren: './list/list.module#ListPageModule'
-  // },
-  // { path: 'deals', loadChildren: './pages/deals/deals.module#DealsPageModule' },
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'cart', loadChildren: './pages/cart/cart.module#CartPageModule' },
    { path: 'fooditems', loadChildren: './pages/fooditems/fooditems.module#FooditemsPageModule' },
-  { path: 'fooddetails', loadChildren: './pages/fooddetails/fooddetails.module#FooddetailsPageModule' },
-  // { path: 'info', loadChildren: './pages/info/info.module#InfoPageModule' }
+  { path: 'fooddetails', loadChildren: './pages/fooddetails/fooddetails.module#FooddetailsPageModule' }
 ];
 
 @NgModule({
